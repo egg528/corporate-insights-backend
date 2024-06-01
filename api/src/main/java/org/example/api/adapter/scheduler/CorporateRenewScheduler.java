de269e4a1b499af6b75c82a49478a0d937c8164d
wkwon <rnjsdntjr26@daum.net>
2024-06-02 00:08:24 +0900

    refactor 변수명 오타 수정

--- api/src/main/java/org/example/api/adapter/scheduler/CorporateRenewScheduler.java
+++ api/src/main/java/org/example/api/adapter/scheduler/CorporateRenewScheduler.java
@@ -8,10 +8,10 @@ import org.springframework.stereotype.Component;
 @Component
 @RequiredArgsConstructor
 public class CorporateRenewScheduler {
-    private final RenewCorporateUseCase renewCorporatesUseCase;
+    private final RenewCorporateUseCase renewCorporateUseCase;
 
     @Scheduled(cron = "0 0 10 * * ?", zone = "Asia/Seoul")
-    public void renewCorporates() {
-        renewCorporatesUseCase.renewAll();
+    public void renewAllCorporates() {
+        renewCorporateUseCase.renewAll();
     }
 }
