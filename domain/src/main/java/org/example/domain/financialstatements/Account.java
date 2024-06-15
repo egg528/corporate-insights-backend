800c3bfcf4842eb95d572f9261f3ec77a39b6788
wkwon <rnjsdntjr26@daum.net>
2024-06-16 00:13:49 +0900

    feat(#11): FinancialStatement, Account getter, builder, allargsconstructor 추가

--- domain/src/main/java/org/example/domain/financialstatements/Account.java
+++ domain/src/main/java/org/example/domain/financialstatements/Account.java
@@ -1,7 +1,14 @@
 package org.example.domain.financialstatements;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
 import java.math.BigDecimal;
 
+@Getter
+@Builder
+@AllArgsConstructor
 public class Account {
     private Long id;
     private Long financialStatementId;
--- domain/src/main/java/org/example/domain/financialstatements/FinancialStatement.java
+++ domain/src/main/java/org/example/domain/financialstatements/FinancialStatement.java
@@ -1,8 +1,15 @@
 package org.example.domain.financialstatements;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
 import java.time.Year;
 import java.util.List;
 
+@Getter
+@Builder
+@AllArgsConstructor
 public class FinancialStatement {
     private Long id;
     private String stockCode;
